84ae040467bd30a6b5d4f47cb8b2327a3cd856d8
ADRIANSYAH <109329296+drian-o@users.noreply.github.com>
2025-04-05 01:52:27 +0700

    Update tinymanagerBC.php

--- tinymanagerBC.php
+++ tinymanagerBC.php
@@ -26,8 +26,8 @@ $use_auth = true;
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
 // Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
-    'admin' => '$2y$10$TTORk9K95uBBa9PwZt8byeAYNe1EkkabtXnQAGTHeOOBHZrBO0nxG', //superbone
-    'user' => '$2y$10$.LdhlB3RwHn4NESki5qewepSS4NZB9QXuvIsUgG2dcHINaRcUp4Um' //1337Arabsanz
+    'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
+    'user' => '$2y$10$TTORk9K95uBBa9PwZt8byeAYNe1EkkabtXnQAGTHeOOBHZrBO0nxG' //superbone
 );
 
 // Readonly users
